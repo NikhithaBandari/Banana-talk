8715d6d03c046e48868d340f12eb1f959825aee4
nikhithabandari <nikhithabandari2002@gmail.com>
2023-07-15 11:49:00 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text){
-    return serverURL + "?"+ "text"+text
+function getTranslationURL(input){
+    var encodedURI = encodeURI(input);
+    return `${serverURL}?text=${encodedURI}`;
 }
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server");
+}
+
 function clickEventHanlder(){
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json =>{var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;}
+    )
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickEventHanlder);
